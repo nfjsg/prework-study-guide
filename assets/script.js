bffec04e34b40b7671c87aa2e72cc3a6355fae0d
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-07-31 17:59:58 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
-*var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-<<<<<<< HEAD
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 function listTopics() {
@@ -26,23 +25,3 @@ console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
 selectTopic();
-=======
-
-for (var x = 0; x < topics.length; x++) {
- console.log(topics[x]);
-}
-
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-}
-*/
->>>>>>> ab6bbef4db168986f4df26ec60e6af97310f13e0
